89e2fd66f0fea8a3d5e6f3a6d660abde3dac2450
kaixuan dai <37492695+kkj154393476@users.noreply.github.com>
2020-10-21 09:27:09 +0800

    Update bootstrap.py

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -292,7 +292,7 @@ def main():
         pip_flags.append('--editable')
     tljh_repo_path = os.environ.get(
         'TLJH_BOOTSTRAP_PIP_SPEC',
-        'git+https://github.com/jupyterhub/the-littlest-jupyterhub.git'
+        'git+https://gitee.com/jiangroubao/the-littlest-jupyterhub.git'
     )
 
     # Upgrade pip
